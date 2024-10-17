865606af76b57828e5039c2fdf60a0a02d4da1aa
Filippe Lins <felippelins09@gmail.com>
2024-10-17 14:12:00 -0300

    att

--- estoque-back/src/main/java/com/seuapp/estoque/EstoqueServer.java
+++ estoque-back/src/main/java/com/seuapp/estoque/EstoqueServer.java
@@ -1,7 +1,6 @@
 package com.seuapp.estoque;
 
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import java.util.EnumSet;
